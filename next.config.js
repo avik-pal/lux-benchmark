aae77f4a2c4208dd4fcb54e3e2f5fcd56062fc63
Songchen Tan <i@tansongchen.com>
2023-02-17 19:09:40 -0500

    Add README

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    runtime: "edge",
+  images: {
+    unoptimized: true,
   },
   compiler: {
     styledComponents: true,
