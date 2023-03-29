442b0053130b4d296800632bed1444025ab9ac28
Avik Pal <avik.pal.2017@gmail.com>
2023-03-29 20:57:39 +0000

    Testing

--- configs/LuxLib.jl.tsx
+++ configs/LuxLib.jl.tsx
@@ -4,39 +4,33 @@ import { Bar, Line } from "react-chartjs-2";
 
 
 export function ChartsLuxLib({ suite }: { suite: BenchmarkGroup }) {
-  const { scalar, mlp, taylor_expansion, pinn } = suite.data;
-  const { forwarddiff: scalar_f, taylordiff: scalar_t } = (scalar as G1).data;
-  const { forwarddiff: mlp_f, taylordiff: mlp_t } = (mlp as G1).data;
-  const { taylordiff: te_t, taylorseries: te_s } = (taylor_expansion as G0)
-    .data;
-  const { taylordiff: pinn_t, finitediff: pinn_f } = (pinn as G1).data;
+  const { groupnorm } = suite.data;
+  const { "Flux CPU": gn_f_cpu, "LuxLib CPU": gn_luxlib_cpu } = (groupnorm as G1).data;
   return (
     <ChartsContainer>
       <ChartWrapper url="https://github.com/LuxDL/LuxLib.jl/blob/main/benchmark/src/groupnorm.jl">
-        <Line
+        <Bar
           data={{
-            labels: Object.keys(scalar_f.data),
+            labels: ["Time"],
             datasets: [
               {
-                data: Object.values(scalar_f.data).map((x) => x.time),
-                label: "ForwardDiff.jl",
+                data: Object.values(gn_f_cpu.data).map((x) => x.time),
+                label: "Flux.jl CPU",
               },
               {
-                data: Object.values(scalar_t.data).map((x) => x.time),
-                label: "TaylorDiff.jl",
+                data: Object.values(gn_luxlib_cpu.data).map((x) => x.time),
+                label: "LuxLib.jl CPU",
               },
             ],
           }}
           options={{
             scales: {
-              x: { title: { text: "Differentiation Order", display: true } },
               y: {
                 title: { text: "Time / ns", display: true },
-                type: "logarithmic",
               },
             },
             plugins: {
-              title: { text: "Scalar function derivatives", display: true },
+              title: { text: "Group Normalization", display: true },
             },
           }}
         />
